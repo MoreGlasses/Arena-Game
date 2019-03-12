342795e175a6381f68fe1def33c9aaac4b3b0828
moreglasses@gmail.com <ABCde111ABCde111>
2019-03-12 19:45:10 +0000

    Player, Zombie Enemy and basic Attack

---
+++ Advanced Maths and Game AI/Assets/Characters/Zombie/ZombieScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieScript : MonoBehaviour
+{
+    //public float speed;
+    //public Transform player;
+
+    //private void FixedUpdate()
+    //{
+    //    float z = Mathf.Atan2((player.transform.position.y - transform.position.y), (player.transform.position.x - transform.position.x)) * Mathf.Rad2Deg - 90;
+
+    //    transform.eulerAngles = new Vector3(0, 0, z);
+
+    //    GetComponent<Rigidbody2D>().AddForce(gameObject.transform.up * speed);
+
+    //}
+
+    
+
+    public Transform target;
+    public float speed = 2f;
+    private float minDistance = 1f;
+    public bool facingRight = true;
+    private float range;
+    
+
+    void Update()
+    {
+        range = Vector2.Distance(transform.position, target.position);
+
+        if (range > minDistance)
+        {
+            Debug.Log(range);
+
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+            //Vector3 lookVector = target.transform.position - transform.position;
+            //lookVector.y = transform.position.y;
+            //Quaternion rot = Quaternion.LookRotation(lookVector);
+            //transform.rotation = Quaternion.Slerp(transform.rotation, rot, 1);
+
+        }
+    }
+
+    }
+
+
+
---
+++ Advanced Maths and Game AI/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Animator animator;
+    
+    public float speed = 50f;
+    float walkinghorizontal, walkingvertical;
+    public bool facingRight = true;
+    public float RotateSpeed = 3.0F;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+    }
+ 
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetTrigger("Attack");
+        }
+
+        walkinghorizontal = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+        walkingvertical = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
+
+
+        //Player to move left, right, up, down
+        transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * speed);
+        transform.Translate(Vector3.up * Input.GetAxis("Vertical") * Time.deltaTime * speed, Space.World);
+
+
+        if (walkinghorizontal < 0 && facingRight) Flip();
+        if (walkinghorizontal > 0 && !facingRight) Flip();
+
+        if (walkinghorizontal != 0 || walkingvertical !=0 )
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+        
+
+    }
+
+    
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+}
